4948534c23f6c1e244c8cdcf88ed2d185aca8f09
Уладзімір Снежка <flashro561@gmail.com>
2020-06-14 00:14:31 +0300

    cart manipulation implemented

--- Pages/Cart.cshtml.cs
+++ Pages/Cart.cshtml.cs
@@ -12,24 +12,6 @@ namespace Pages.Model
 {
     public class CartModel : PageModel
     {
-        private IRepository _repo;
 
-        public CartModel(IRepository repo)
-        {
-            _repo = repo;
-        }
-        public Dictionary<Item, int> CartItems { get; set; } = new Dictionary<Item, int>();
-        public void OnGet()
-        {
-            var sessionCart = HttpContext.Session.Get<Dictionary<int, int>>("Cart");
-            if (sessionCart != null)
-            {
-                foreach (var key in sessionCart.Keys)
-                {
-                    var item = _repo.GetItem(key);
-                    CartItems[item] = sessionCart[key];
-                }
-            }
-        }
     }
 }
